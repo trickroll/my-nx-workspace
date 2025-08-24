79ac61e7eba0caaafd3548218e029ef2c9860a17
trickroll <mwd244@nyu.edu>
2025-08-24 12:20:42 +0800

    refactor(tests): comment out type check and assert expected return value

--- packages/zoo/src/lib/zoo.spec.ts
+++ packages/zoo/src/lib/zoo.spec.ts
@@ -2,6 +2,7 @@ import { zoo } from './zoo.js';
 
 describe('zoo', () => {
   it('should work', () => {
-    expect(typeof zoo()).toBe('string');
+    // expect(typeof zoo()).toBe('string');
+    expect(zoo()).toBe('zoo');
   });
 });
